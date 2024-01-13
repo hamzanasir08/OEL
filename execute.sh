8e20599cd25659628b591001c77338c61be4778d
Abdus Samad <aadi64499@gmail.com>
2024-01-13 16:54:19 +0500

    Script to run program

---
+++ execute.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Compiling all the C files
+gcc -c send_email.c -o send_email.o
+gcc -c convert_from_json.c -o convert_from_json.o
+gcc -c calculate_averages.c -o calculate_averages.o
+gcc -c heading.c -o heading.o
+gcc -c main.c -o main.o
+gcc main.o send_email.o convert_from_json.o calculate_averages.o heading.o -o executable -lcurl -ljansson
+#Run the compiled program
+./executable
+#clean up the compiled object files
+rm send_email.o convert_from_json.o calculate_averages.o heading.o
